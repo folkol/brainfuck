3c80e06d82c1e2095c66940186f81d8a0373945c
Matte <mattias4@kth.se>
2016-07-17 18:57:11 +0200

    Some OP codes implemented. No branches yet.

---
+++ src/main/java/com/folkol/interpreters/Interpreter.java
@@ -0,0 +1,51 @@
+package com.folkol.interpreters;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class Interpreter
+{
+    public static void main(String[] args) throws IOException
+    {
+        byte[] program = Files.readAllBytes(Paths.get(args[0]));
+        exec(program, System.in, System.out);
+    }
+
+    public static void exec(byte[] program, InputStream in, PrintStream out) throws IOException
+    {
+        int[] cells = new int[30000];
+        int pc = 0;
+        int dp = 0;
+        while (pc < program.length) {
+            switch (program[pc]) {
+                case '>':
+                    dp++;
+                    break;
+                case '<':
+                    dp--;
+                    break;
+                case '+':
+                    cells[dp]++;
+                    break;
+                case '-':
+                    cells[dp]--;
+                    break;
+                case '.':
+                    out.print((char) cells[dp]);
+                    break;
+                case ',':
+                    cells[dp] = in.read();
+                    break;
+                case '[':
+                    throw new RuntimeException("Not yet implemented!");
+                case ']':
+                    throw new RuntimeException("Not yet implemented!");
+            }
+            pc++;
+        }
+    }
+}
---
+++ src/test/java/com/folkol/interpreters/InterpreterTest.java
@@ -0,0 +1,28 @@
+package com.folkol.interpreters;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class InterpreterTest
+{
+    @Test
+    public void helloWorld()
+        throws Exception
+    {
+        byte[] program = (
+            "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++." +
+            "+++++++++++++++++++++++++++++.+++++++..+++.------------------------------" +
+            "-------------------------------------.------------.++++++++++++++++++++++" +
+            "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.-------" +
+            "-.+++.------.--------.---------------------------------------------------" +
+            "----------------.").getBytes();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        Interpreter.exec(program, null, new PrintStream(baos));
+
+        Assert.assertEquals("Hello, world!", baos.toString());
+    }
+}
